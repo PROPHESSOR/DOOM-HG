970fa9c122965f77a87ffd4046676e1d3dd8cbbc
Morthimer McMare <morthimer_second@list.ru>
2018-07-03 00:54:47 +0300

    Сделал SummonerSpot для оригинального Doom'а.
    Сделал SummonerSpot для оригинального Doom'а -- выдаёт случайную(-ые) вещь (вещи) из Medikid, Stimpack и/или HealthBonus. Остальные можно сделать по аналогии. Если никто не захочет -- придётся опять мне...

--- DECORATE.c
+++ DECORATE.c
@@ -1,10 +1,15 @@
 #include "actors/items/utils.java"
+
 #include "actors/utils/sentry.java"
 #include "actors/utils/support.java"
+
 #include "actors/spots/healthspot.java"
 #include "actors/spots/armorspot.java"
 #include "actors/spots/ammospot.java"
+#include "actors/spots/McMareSpots.txt"
+
 #include "actors/items/test.java"
+
 // #include "actors/items.java"
 // #include "actors/itemspot.txt"
 // #include "actors/allies.txt"
